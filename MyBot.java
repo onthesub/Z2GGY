9b40bb990351aa72edf06bcc8c75c8d0e79d1453
Jesus Chris <LoungeBot.Awesomecougars@gmail.com>
2012-08-16 03:29:49 +0200

    commit 1

---
+++ MyBot.java
@@ -0,0 +1,37 @@
+import org.jibble.pircbot.*;
+import java.util.*;
+
+public class MyBot extends PircBot {
+
+
+    String derpDJ = "ImaginaryDJ";
+    String herpDJ = "HypotheticalSoundSystem";
+	HashMap kudos = new HashMap();
+	kudos.put( "derp", new HashMap() ) ;
+	kudos.put( "herp", new HashMap() ) ;
+
+
+    public MyBot() {
+        this.setName("Z2GGY");
+    }
+	
+    public void onMessage(String channel, String sender,
+                       String login, String hostname, String message) {
+        
+        
+        if( message.toLowerCase().startsWith("!kudos derp") )
+        {
+          derpKudos.add(sender);
+          sendMessage("NickServ", "info " + sender);
+        }
+        if( message.toLowerCase().startsWith("!kudos herp") )
+        {
+          herpKudos.add(sender);
+          sendMessage("NickServ", "info " + sender);
+        }
+        
+        
+    }
+                        
+
+}
