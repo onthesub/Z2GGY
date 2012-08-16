bd7645f4b6adfa93094a192240f7aa1502877cf2
otis <otisub@gmail.com>
2012-08-15 20:35:41 -0500

    bracket and tab consistency

--- MyBot.java
+++ MyBot.java
@@ -2,36 +2,25 @@ import org.jibble.pircbot.*;
 import java.util.*;
 
 public class MyBot extends PircBot {
-
-
-    String derpDJ = "ImaginaryDJ";
-    String herpDJ = "HypotheticalSoundSystem";
+  String derpDJ = "ImaginaryDJ";
+  String herpDJ = "HypotheticalSoundSystem";
 	HashMap kudos = new HashMap();
 	kudos.put( "derp", new HashMap() ) ;
 	kudos.put( "herp", new HashMap() ) ;
 
 
-    public MyBot() {
-        this.setName("Z2GGY");
-    }
+  public MyBot() {
+    this.setName("Z2GGY");
+  }
 	
-    public void onMessage(String channel, String sender,
-                       String login, String hostname, String message) {
-        
-        
-        if( message.toLowerCase().startsWith("!kudos derp") )
-        {
-          derpKudos.add(sender);
-          sendMessage("NickServ", "info " + sender);
-        }
-        if( message.toLowerCase().startsWith("!kudos herp") )
-        {
-          herpKudos.add(sender);
-          sendMessage("NickServ", "info " + sender);
-        }
-        
-        
+  public void onMessage(String channel, String sender, String login, String hostname, String message) {
+    if (message.toLowerCase().startsWith("!kudos derp") ) {
+      derpKudos.add(sender);
+      sendMessage("NickServ", "info " + sender);
     }
-                        
-
+    if( message.toLowerCase().startsWith("!kudos herp") {
+      herpKudos.add(sender);
+      sendMessage("NickServ", "info " + sender);
+    }
+  }
 }
