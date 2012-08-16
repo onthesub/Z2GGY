9299b5b1ed018c38b1fc5eda76d44ded9d437029
Jesus Chris <LoungeBot.Awesomecougars@gmail.com>
2012-08-16 08:15:18 +0200

    testing

--- MyBot.java
+++ MyBot.java
@@ -6,11 +6,11 @@ import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.managers.ListenerManager;
 import java.util.*;
 
-public class MyBot extends PircBotX {
+public class MyBot extends ListenerAdapter implements Listener   {
 
      
         HashMap<String,String> dj = new HashMap<String,String>();   
-		HashMap<String,ArrayList> kudos = new HashMap<String,ArrayList>();
+		HashMap<String,ArrayList<String>> kudos = new HashMap<String,ArrayList<String>>();
 
 
     public MyBot() {         
@@ -18,17 +18,17 @@ public class MyBot extends PircBotX {
 		dj.put( "herp", "HypotheticalSoundSystem" );
 		kudos.put( "derp", new ArrayList<String>() );
 		kudos.put( "herp", new ArrayList<String>() );
-        this.setName("Z2GGY");
-    }
+    }             
 	
-    public void onMessage(MessageEvent event) throws Exception {
+    public void onMessage(MessageEvent event) throws Exception
+	{
         
-        if( event.getMessage().equalsIgnoreCase("!kudos derp") | event.getMessage().equalsIgnoreCase("!kudos herp") )
+        if( event.getMessage().equalsIgnoreCase("!kudos derp") || event.getMessage().equalsIgnoreCase("!kudos herp") )
         {
 		  String target = event.getMessage().toLowerCase().split(" ")[1];
-		  String user = event.getUser().getHostmask();
+		  String userhost = event.getUser().getHostmask();
 		  
-		  if( kudos.get( target ).contains( user ) ) 
+		  if( kudos.get( target ).contains( userhost ) ) 
 		  {
 		   sendNotice( event.getUser(), "You have already given kudos to " + dj.get( target ) + " during this session.");
 		  }
@@ -36,13 +36,16 @@ public class MyBot extends PircBotX {
 		  else 
 		  
 		  {
-           kudos.get( target ).add( user );
+           kudos.get( target ).add( userhost );
            sendMessage("#AwesomeCougars", event.getUser() + " has given kudos to " + dj.get( target ) + ". (" + kudos.get( target ).size() + " kudos)");
 		  }
         }
+		
+		else if( event.getMessage().toLowerCase().startsWith("go away Z2GGY") &&  event.getUser().isIrcop() )
+		{
+			quitServer("Goodbye, " + event.getUser().getNick() + "!");
+		}
         
-        
-    }
-                        
+    }  
 
 }
