d4b16765523bacd6ec4996f873a2521c1175f54b
Jesus Chris <LoungeBot.Awesomecougars@gmail.com>
2012-08-16 07:19:23 +0200

    updated to pircbotx

--- MyBot.java
+++ MyBot.java
@@ -1,33 +1,44 @@
-import org.jibble.pircbot.*;
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.Event;
+import org.pircbotx.hooks.Listener;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.managers.ListenerManager;
 import java.util.*;
 
-public class MyBot extends PircBot {
+public class MyBot extends PircBotX {
+
+     
+        HashMap<String,String> dj = new HashMap<String,String>();   
+		HashMap<String,ArrayList> kudos = new HashMap<String,ArrayList>();
 
 
-    String derpDJ = "ImaginaryDJ";
-    String herpDJ = "HypotheticalSoundSystem";
-	HashMap kudos = new HashMap();
-	kudos.put( "derp", new HashMap() ) ;
-	kudos.put( "herp", new HashMap() ) ;
-
-
-    public MyBot() {
+    public MyBot() {         
+		dj.put( "derp", "Imaginarydj" );
+		dj.put( "herp", "HypotheticalSoundSystem" );
+		kudos.put( "derp", new ArrayList<String>() );
+		kudos.put( "herp", new ArrayList<String>() );
         this.setName("Z2GGY");
     }
 	
-    public void onMessage(String channel, String sender,
-                       String login, String hostname, String message) {
+    public void onMessage(MessageEvent event) throws Exception {
         
-        
-        if( message.toLowerCase().startsWith("!kudos derp") )
+        if( event.getMessage().equalsIgnoreCase("!kudos derp") | event.getMessage().equalsIgnoreCase("!kudos herp") )
         {
-          derpKudos.add(sender);
-          sendMessage("NickServ", "info " + sender);
-        }
-        if( message.toLowerCase().startsWith("!kudos herp") )
-        {
-          herpKudos.add(sender);
-          sendMessage("NickServ", "info " + sender);
+		  String target = event.getMessage().toLowerCase().split(" ")[1];
+		  String user = event.getUser().getHostmask();
+		  
+		  if( kudos.get( target ).contains( user ) ) 
+		  {
+		   sendNotice( event.getUser(), "You have already given kudos to " + dj.get( target ) + " during this session.");
+		  }
+		  
+		  else 
+		  
+		  {
+           kudos.get( target ).add( user );
+           sendMessage("#AwesomeCougars", event.getUser() + " has given kudos to " + dj.get( target ) + ". (" + kudos.get( target ).size() + " kudos)");
+		  }
         }
         
         
