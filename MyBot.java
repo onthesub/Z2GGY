c5e300efe9401e7492bf1f278104fd92297d37d1
otis <otisub@gmail.com>
2012-08-16 12:25:51 -0500

    Merge remote tracking branch

--- MyBot.java
+++ MyBot.java
@@ -6,71 +6,46 @@ import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.managers.ListenerManager;
 import java.util.*;
 
-<<<<<<< HEAD
-public class MyBot extends PircBot {
-  String derpDJ = "ImaginaryDJ";
-  String herpDJ = "HypotheticalSoundSystem";
-	HashMap kudos = new HashMap();
-	kudos.put( "derp", new HashMap() ) ;
-	kudos.put( "herp", new HashMap() ) ;
-
-
-  public MyBot() {
-    this.setName("Z2GGY");
-  }
-	
-  public void onMessage(String channel, String sender, String login, String hostname, String message) {
-    if (message.toLowerCase().startsWith("!kudos derp") ) {
-      derpKudos.add(sender);
-      sendMessage("NickServ", "info " + sender);
-    }
-    if( message.toLowerCase().startsWith("!kudos herp") ) {
-      herpKudos.add(sender);
-      sendMessage("NickServ", "info " + sender);
-    }
-  }
-=======
 public class MyBot extends ListenerAdapter implements Listener   {
 
      
         HashMap<String,String> dj = new HashMap<String,String>();   
-		HashMap<String,ArrayList<String>> kudos = new HashMap<String,ArrayList<String>>();
+    HashMap<String,ArrayList<String>> kudos = new HashMap<String,ArrayList<String>>();
 
 
     public MyBot() {         
-		dj.put( "derp", "Imaginarydj" );
-		dj.put( "herp", "HypotheticalSoundSystem" );
-		kudos.put( "derp", new ArrayList<String>() );
-		kudos.put( "herp", new ArrayList<String>() );
+    dj.put( "derp", "Imaginarydj" );
+    dj.put( "herp", "HypotheticalSoundSystem" );
+    kudos.put( "derp", new ArrayList<String>() );
+    kudos.put( "herp", new ArrayList<String>() );
     }             
-	
+  
     public void onMessage(MessageEvent event) throws Exception
-	{
+  {
         
         if( event.getMessage().equalsIgnoreCase("!kudos derp") || event.getMessage().equalsIgnoreCase("!kudos herp") )
         {
-		  String target = event.getMessage().toLowerCase().split(" ")[1];
-		  String userhost = event.getUser().getHostmask();
-		  
-		  if( kudos.get( target ).contains( userhost ) ) 
-		  {
-		   sendNotice( event.getUser(), "You have already given kudos to " + dj.get( target ) + " during this session.");
-		  }
-		  
-		  else 
-		  
-		  {
+      String target = event.getMessage().toLowerCase().split(" ")[1];
+      String userhost = event.getUser().getHostmask();
+      
+      if( kudos.get( target ).contains( userhost ) ) 
+      {
+       sendNotice( event.getUser(), "You have already given kudos to " + dj.get( target ) + " during this session.");
+      }
+      
+      else 
+      
+      {
            kudos.get( target ).add( userhost );
            sendMessage("#AwesomeCougars", event.getUser() + " has given kudos to " + dj.get( target ) + ". (" + kudos.get( target ).size() + " kudos)");
-		  }
+      }
         }
-		
-		else if( event.getMessage().toLowerCase().startsWith("go away Z2GGY") &&  event.getUser().isIrcop() )
-		{
-			quitServer("Goodbye, " + event.getUser().getNick() + "!");
-		}
+    
+    else if( event.getMessage().toLowerCase().startsWith("go away Z2GGY") &&  event.getUser().isIrcop() )
+    {
+      quitServer("Goodbye, " + event.getUser().getNick() + "!");
+    }
         
     }  
 
->>>>>>> upstream/master
 }
